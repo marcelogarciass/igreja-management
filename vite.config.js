93217dc49ddba7324f4e82efbe32931adbb94fa2
marcelogarciass <marcelogarciass@hotmail.com>
2025-04-10 19:59:39 -0300

    0.0.7

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,21 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
-    'process.env': {
-      BASE_URL: JSON.stringify('/')
-    }
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
-  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
-    chunkSizeWarningLimit: 1600
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
