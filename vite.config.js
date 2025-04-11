d4312e4ecba7346d8e9aa13f984879023b2cc1d9
marcelogarciass <marcelogarciass@hotmail.com>
2025-04-10 21:35:51 -0300

    0.0.9

--- vite.config.js
+++ vite.config.js
@@ -6,42 +6,38 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-  plugins: [vue()],
-  define: {
-    'process.env': {
-      BASE_URL: JSON.stringify('/'),
-      NODE_ENV: JSON.stringify(mode),
-      VITE_SUPABASE_URL: JSON.stringify(env.VITE_SUPABASE_URL),
-      VITE_SUPABASE_ANON_KEY: JSON.stringify(env.VITE_SUPABASE_ANON_KEY)
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use 'sass:math' as *;`
+    plugins: [vue()],
+    define: {
+      'process.env': env
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use 'sass:math' as *;`
+        }
       }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'esbuild',
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return 'vendor';
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      sourcemap: false,
+      minify: 'esbuild',
+      chunkSizeWarningLimit: 1600,
+      target: 'esnext',
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
           }
         }
       }
     }
-  }
   }
 })
