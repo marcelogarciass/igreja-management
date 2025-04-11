16f5bffed5bf79e7e6c610bbfd235aeba4df7076
marcelogarciass <marcelogarciass@hotmail.com>
2025-04-10 21:26:34 -0300

    0.0.8

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
   plugins: [vue()],
+  define: {
+    'process.env': {
+      BASE_URL: JSON.stringify('/'),
+      NODE_ENV: JSON.stringify(mode),
+      VITE_SUPABASE_URL: JSON.stringify(env.VITE_SUPABASE_URL),
+      VITE_SUPABASE_ANON_KEY: JSON.stringify(env.VITE_SUPABASE_ANON_KEY)
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use 'sass:math' as *;`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
@@ -17,8 +35,13 @@ export default defineConfig({
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
       }
     }
+  }
   }
 })
